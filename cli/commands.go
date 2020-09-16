a8b90bc39b461763766754de7dd614da12cf2043
Mikolaj Figurski <meek.f80@gmail.com>
2020-09-16 01:10:37 -0400

    Remove unnecessary functions

--- cli/commands.go
+++ cli/commands.go
@@ -1,20 +1,10 @@
 package cli
 
 import (
-	"errors"
 	"fmt"
 )
 
 func helpCommand() error {
 	fmt.Println("Please refer to the repository readme for usage")
 	return nil
-}
-
-func errorCommand(m string) error {
-	fmt.Println(m)
-	return errors.New(m)
-}
-
-func dneCommand(c string) error {
-	return errorCommand(fmt.Sprintf("The command '%s' does not exist", c))
 }
